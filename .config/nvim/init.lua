2d03332c078e7e461c90e93cf7137e82fe09431e
mcreekmore <matthewacreekmore@gmail.com>
2024-04-30 20:16:05 -0400

    add telescope harpoon marks keybind

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -434,6 +434,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 			vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "[S]earch [M]arks" })
+			vim.keymap.set("n", "<leader>sa", ":Telescope harpoon marks<CR>", { desc = "[S]earch [M]arks" })
 
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
