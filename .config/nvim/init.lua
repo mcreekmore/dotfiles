12761790c916b34d4eb68be97aefba64d94bf241
mcreekmore <matthewacreekmore@gmail.com>
2024-04-30 20:07:51 -0400

    add <leader>sm to telescope marks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -433,6 +433,7 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+			vim.keymap.set("n", "<leader>sm", builtin.marks, { desc = "[S]earch [M]arks" })
 
 			-- Slightly advanced example of overriding default behavior and theme
 			vim.keymap.set("n", "<leader>/", function()
