e570e4c59f35b89b7e62e34bf5f8441daa7ad0a9
mcreekmore <matthewacreekmore@gmail.com>
2024-05-16 13:55:19 -0400

    tab width 4

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -77,6 +77,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 15
 
+-- tab width
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
