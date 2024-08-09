deccb288d70e7b80bc45cbf6dd8918832498f852
mcreekmore <matthewacreekmore@gmail.com>
2024-08-09 18:29:11 -0400

    add ssh profile and wezterm initial config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,17 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("Hack Nerd Font Mono")
+config.font_size = 18
 
--- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
+config.ssh_domains = {
+	{
+		name = "unraid",
+		remote_address = "192.168.1.207",
+		username = "root",
+	},
+}
 
--- and finally, return the configuration to wezterm
 return config
