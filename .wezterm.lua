2c2becef856550105b191d0f1a04d8b773ed269d
mcreekmore <matthewacreekmore@gmail.com>
2024-08-10 14:36:22 -0400

    paste keybind for wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+local act = wezterm.action
 local launch_menu = {}
 
 config.color_scheme = "Catppuccin Mocha"
@@ -10,6 +11,10 @@ config.hide_tab_bar_if_only_one_tab = true
 config.scrollback_lines = 10000
 config.use_fancy_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
+
+config.keys = {
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+}
 
 config.ssh_domains = {
 	{
@@ -31,6 +36,7 @@ config.wsl_domains = {
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_domain = "WSL:Ubuntu"
 	config.font_size = 12
+	config.window:set_inner_size(100, 100)
 
 	table.insert(launch_menu, {
 		label = "PowerShell",
