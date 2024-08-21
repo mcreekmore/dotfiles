1d8ef296510ae5cce03269ec3676df5e8c5ea375
mcreekmore <matthewacreekmore@gmail.com>
2024-08-21 11:58:32 -0400

    first commit of windows setup script

---
+++ scripts/windows/setup.ps1
@@ -0,0 +1,67 @@
+$local_packages = @(
+    "7zip"
+    "audacity"
+    "autohotkey"
+    "bitwarden"
+    "discord"
+    "element"
+    "firefox"
+    "git"
+    "gimp"
+    "googlechrome"
+    "okibcn_ScoopMaster/googledrive-np_(1)"
+    "nerd-fonts/Hack-NF-Mono"
+    "hwmonitor"
+    "keepassxc"
+    "microsoft-edge"
+    "nonportable/mullvadvpn-np"
+    "msiafterburner"
+    "okibcn_ScoopMaster/nvidia-geforce-experience-np"
+    "obsidian"
+    "nonportable/parsec-np"
+    "playnite"
+    "okibcn_ScoopMaster/plexamp_(1)"
+    "plex-desktop"
+    "slack"
+    "soundswitch"
+    "spotify"
+    "signal"
+    "okibcn_ScoopMaster/slippi-launcher_(1)"
+    "steam"
+    "sudo"
+    "tailscale"
+    "teamviewer"
+    "telegram"
+    "vscode"
+    "wezterm"
+    "whatsapp"
+    "windows-terminal"
+    "zoom"
+)
+
+# $global_packages = @(
+# )
+
+# set scoop env path
+[environment]::setEnvironmentVariable('SCOOP','C:\scoop','User')
+
+# install scoop
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+
+# enable multi-connection downloads
+# scoop install aria2
+# scoop config aria2-enabled true
+
+# add scoop buckets
+scoop bucket add extras
+scoop bucket add okibcn_ScoopMaster https://github.com/okibcn/ScoopMaster
+scoop bucket add nerd-fonts
+scoop bucket add nonportable
+scoop bucket add versions
+
+# install packages using scoop
+scoop install $local_packages
+
+# install contexts to registry
+& $env:SCOOP\apps\7zip\current\install-context.reg
