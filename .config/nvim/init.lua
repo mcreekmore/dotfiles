9afcaa51e3122f33a84892367403fddfa2b2bffe
mcreekmore <matthewacreekmore@gmail.com>
2024-09-09 10:59:56 -0400

    updated nvim 10.1, finally tmux seperator correct

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -212,20 +212,40 @@ require("lazy").setup({
 			require("which-key").setup()
 
 			-- Document existing key chains
-			require("which-key").register({
-				["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-				["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-				["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-				["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-				["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-				["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
-				["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
-				["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
-				["<leader>u"] = { name = "[U]ndotree", _ = "which_key_ignore" },
+			require("which-key").add({
+				-- ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+				-- ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+				-- ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+				-- ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+				-- ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+				-- ["<leader>t"] = { name = "[T]oggle", _ = "which_key_ignore" },
+				-- ["<leader>h"] = { name = "Git [H]unk", _ = "which_key_ignore" },
+				-- ["<leader>g"] = { name = "[G]it", _ = "which_key_ignore" },
+				-- ["<leader>u"] = { name = "[U]ndotree", _ = "which_key_ignore" },
+				{ "<leader>c", group = "[C]ode" },
+				-- { "<leader>c_", hidden = true },
+				{ "<leader>d", group = "[D]ocument" },
+				-- { "<leader>d_", hidden = true },
+				{ "<leader>g", group = "[G]it" },
+				-- { "<leader>g_", hidden = true },
+				{ "<leader>h", group = "Git [H]unk" },
+				-- { "<leader>h_", hidden = true },
+				{ "<leader>r", group = "[R]ename" },
+				-- { "<leader>r_", hidden = true },
+				{ "<leader>s", group = "[S]earch" },
+				-- { "<leader>s_", hidden = true },
+				{ "<leader>t", group = "[T]oggle" },
+				-- { "<leader>t_", hidden = true },
+				{ "<leader>u", group = "[U]ndotree" },
+				-- { "<leader>u_", hidden = true },
+				{ "<leader>w", group = "[W]orkspace" },
+				-- { "<leader>w_", hidden = true },
 			})
 			-- visual mode
-			require("which-key").register({
-				["<leader>h"] = { "Git [H]unk" },
+			require("which-key").add({
+				"<leader>h",
+				desc = "Git [H]unk",
+				mode = "v",
 			}, { mode = "v" })
 		end,
 	},
@@ -265,6 +285,32 @@ require("lazy").setup({
 				},
 			})
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		end,
+	},
+
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		opts = {},
+		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" },
+		config = function()
+			require("render-markdown").setup({
+				code = {
+					-- Determines where language icon is rendered:
+					--  right: right side of code block
+					--  left:  left side of code block
+					position = "left",
+					style = "full",
+				},
+			})
+		end,
+	},
+
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
 		end,
 	},
 
@@ -652,9 +698,12 @@ require("lazy").setup({
 				--    https://github.com/pmizio/typescript-tools.nvim
 				--
 				-- But for many setups, the LSP (`tsserver`) will work just fine
-				tsserver = {},
+				-- tsserver = {},
+				ts_ls = {},
 				--
 				astro = {},
+
+				marksman = {},
 
 				lua_ls = {
 					-- cmd = {...},
