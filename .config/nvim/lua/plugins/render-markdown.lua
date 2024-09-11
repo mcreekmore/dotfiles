7e5a1e28ac2c391a436110accd6ed29eda51cf23
mcreekmore <matthewacreekmore@gmail.com>
2024-09-11 16:25:10 -0400

    removed some comments

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -5,9 +5,6 @@ return {
 	config = function()
 		require("render-markdown").setup({
 			code = {
-				-- Determines where language icon is rendered:
-				--  right: right side of code block
-				--  left:  left side of code block
 				position = "left",
 				style = "full",
 			},
