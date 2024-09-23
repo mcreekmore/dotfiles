93e717d7a725c325b8150f87bca0243a77bce9b0
mcreekmore <matthewacreekmore@gmail.com>
2024-09-23 13:59:26 -0400

    add parsers to treesitter conf

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,7 +2,23 @@ return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	opts = {
-		ensure_installed = { "bash", "c", "html", "lua", "luadoc", "markdown", "vim", "vimdoc" },
+		ensure_installed = {
+			"bash",
+			"c",
+			"css",
+			"go",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"markdown",
+			"python",
+			"rust",
+			"typescript",
+			"vim",
+			"vimdoc",
+		},
 		-- Autoinstall languages that are not installed
 		auto_install = true,
 		highlight = {
