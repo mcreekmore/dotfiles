2f7fdfe8389c492b0ed39802dafeef6be4d07f9b
mcreekmore <matthewacreekmore@gmail.com>
2025-03-19 12:08:40 -0400

    make paths agnostic

--- scripts/executable_setup.sh
+++ scripts/executable_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+OS_PATH=''
 BREW_PATH=''
 OS_NAME=$(uname -s)
 
@@ -8,15 +9,17 @@ if ! type "brew" > /dev/null; then
   /bin/bash -c "$(curl -sfSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
   if [ "$OS_NAME" = "Darwin" ]; then
-    BREW_PATH='/usr/local/bin'
+    OS_PATH='/usr/local/bin'
   elif [ "$OS_NAME" = "Linux" ]; then
-    BREW_PATH='/home/linuxbrew/.linuxbrew/bin'
+    OS_PATH='/home/linuxbrew/.linuxbrew/bin'
   else
     echo "Unsupported perating system"
     exit 1
   fi
+  BREW_PATH='$OS_PATH/brew'
 else
   echo "Homebrew is already installed."
+  OS_PATH=''
   BREW_PATH='brew'
 fi
 
@@ -36,16 +39,16 @@ echo "All packages are installed!"
 # set zsh default shell
 # sudo chsh -s $(which zsh)
 
-$BREW_PATH/chezmoi init --apply https://github.com/mcreekmore/dotfiles.git
+$OS_PATH/chezmoi init --apply https://github.com/mcreekmore/dotfiles.git
 
 if [ "$STATUS" = "unauthenticated" ]; then
     echo "Bitwarden Master Password:"
-    BW_SESSION=$($BREW_PATH/bw login --raw </dev/tty)
+    BW_SESSION=$($OS_PATH/bw login --raw </dev/tty)
     export BW_SESSION="$BW_SESSION"
     echo "Successfully logged in"
 elif [ "$STATUS" = "locked" ]; then
     echo "Bitwarden vault is locked. Please enter your master password to unlock:"
-    BW_SESSION=$($BREW_PATH/bw unlock --raw </dev/tty)
+    BW_SESSION=$($OS_PATH/bw unlock --raw </dev/tty)
     export BW_SESSION="$BW_SESSION"
     echo "Successfully unlocked vault"
 else
